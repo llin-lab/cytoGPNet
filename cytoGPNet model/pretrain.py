325e31362946c5082d27217449f83ed780c51c38
zhangjx1996 <43453519+zhangjx1996@users.noreply.github.com>
2025-04-18 15:02:36 -0400

    Add files via upload
    Modified pretraining and training step with comments addition and code cleaning

--- cytoGPNet model/pretrain.py
+++ cytoGPNet model/pretrain.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 30 04:29:48 2022
+Created on Tue Aug 30 04:29:48 2024
 
 @author: jz259
 """
 
 import torch
-import torch.utils.data
+from torch.utils.data import DataLoader
 from torch import nn, optim
 from torch.autograd import Variable
 
 from loaddata import CyTOF_Dataset
-from AE import Autoencoder, Attention_Layer, avg_pool
+
+from gp_utils import simple_AE
 
 import os
 import argparse
@@ -25,9 +26,12 @@ def setup_args():
 
     options = argparse.ArgumentParser()
 
+    # data directory
+    options.add_argument('-datadir', '--data-dir', action="store", dest="data_dir", default='./HEUvsUE')
+
     # save and directory options
-    options.add_argument('-sd', '--save-dir', action="store", dest="save_dir", default='./eval')
-    options.add_argument('--save-freq', action="store", dest="save_freq", default=20, type=int)
+    options.add_argument('-sd', '--save-dir', action="store", dest="save_dir", default='./ae_output')
+    options.add_argument('--save-freq', action="store", dest="save_freq", default=10, type=int)
     #options.add_argument('--pretrained-file', action="store")
 
     # training parameters
@@ -35,12 +39,14 @@ def setup_args():
     options.add_argument('-w', '--num-workers', action="store", dest="num_workers", default=10, type=int)
     options.add_argument('-lrAE', '--learning-rate-AE', action="store", dest="learning_rate_AE", default=1e-6, type=float)
     options.add_argument('-lrD', '--learning-rate-D', action="store", dest="learning_rate_D", default=1e-6, type=float)
-    options.add_argument('-e', '--max-epochs', action="store", dest="max_epochs", default=1001, type=int)
+    options.add_argument('-e', '--max-epochs', action="store", dest="max_epochs", default=100, type=int)
     options.add_argument('-wd', '--weight-decay', action="store", dest="weight_decay", default=0, type=float)
 
+
     # hyperparameters
-    options.add_argument('--alpha', action="store", default=1., type=float) # weight for classfication loss compared to discriminative loss
-    options.add_argument('--latent-dims', action="store", default=4, type=int) # size of dimension for latent space of autoencoder
+    # options.add_argument('--alpha', action="store", default=1., type=float) # weight for classfication loss compared to discriminative loss
+    options.add_argument('--hidden-dims', action="store", dest="hidden_dims", default=4, type=int)
+    options.add_argument('--latent-dims', action="store", dest="latent_dims", default=2, type=int) # size of dimension for latent space of autoencoder
 
     # gpu options
     options.add_argument('-gpu', '--use-gpu', action="store_false", dest="use_gpu")
@@ -51,138 +57,61 @@ def setup_args():
 args = setup_args()
 if not torch.cuda.is_available():
     args.use_gpu = False
+device = torch.device("cuda" if args.use_gpu else "cpu")
 
 os.makedirs(args.save_dir, exist_ok=True)
 
 
-#============= TRAINING INITIALIZATION ==============
-
-# load data, shape (3, 1, 10000, 24)
-preData = CyTOF_Dataset(datadir="FCS_to_Array", name = "train_preData.obj", mode='train')
-postData = CyTOF_Dataset(datadir="FCS_to_Array",name = "train_postData.obj", mode='train')
-clinic_data = np.array(postData.data[0])[:,1].astype('float')
-# TODO: target_data should be label and it should be either 0 or 1.
-# target_data = np.array(postData.data[0])[:,1].astype('int') # I run into error at this line. Cannot convert str into int.I rewrote it with the correct syntex but the semantic seems wrong.
-# np.array(postData.data[0])[:,1] is ['study1', 'study2', 'study3'], converting it into int is [1, 2, 3], not 0 or 1.
-target_data = np.array([int(i[-1]) for i in np.array(postData.data[0])[:,2]])
-
-p = preData.data[0].shape[1] - 2
-
-preCyTOF_loader = torch.utils.data.DataLoader(torch.from_numpy(preData.data[1]).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-postCyTOF_loader = torch.utils.data.DataLoader(torch.from_numpy(postData.data[1]).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-clinic_loader = torch.utils.data.DataLoader(torch.from_numpy(clinic_data).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-target_laoder = torch.utils.data.DataLoader(torch.from_numpy(target_data).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-
-print("Data loaded.")
 
 
-# initialize autoencoder
-# TODO: maybe let netPre1 and netPost1 share parameters; let netPre2 and netPost2 share parameters (Do this later).
-# TODO: since you add the pooling layer to the classifier, you can still use the reconstruction error for these encoders  (Do this later).
+# --------- Load and process data ---------
+dataset = CyTOF_Dataset(datadir=args.data_dir, name="train_Data.obj", mode='train')
+cyto_tensor = torch.from_numpy(dataset.data[1]).float()  # (n_samples, n_channels, n_cells, n_markers)
 
-netAE = Autoencoder(n_input=preData.data[1].shape, nz=args.latent_dims) # common encoder
-#netPre2 = Autoencoder(n_input=preData.data[1].shape, nz=args.latent_dims) # specific encoder
-netAtt = Attention_Layer() # common encoder
-#netPost2 = Autoencoder(n_input=postData.data[1].shape, nz=args.latent_dims) # specific encoder
-netClf = Simple_Classifier(nz = p + 2 * args.latent_dims)
+n_samples, n_channels, n_cells, n_markers = cyto_tensor.shape
+
+# Concatenate along the cell axis: (n_samples, n_channels * n_cells, n_markers)
+cyto_concat = cyto_tensor.permute(0, 2, 1, 3).reshape(n_samples, n_channels * n_cells, n_markers)
+
+# Flatten everything for AE training: (total_cells, n_markers)
+cyto_flat = cyto_concat.reshape(-1, n_markers)
+
+# Create a DataLoader for training
+data_loader = DataLoader(cyto_flat, batch_size=args.batch_size, shuffle=True)
+
+# --------- Initialize model, loss, optimizer ---------
+ae_model = simple_AE(input_dim=n_markers, embed_dim=args.latent_dims, hidden_dim=args.hidden_dims)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(ae_model.parameters(), lr=args.learning_rate_AE)
 
 if args.use_gpu:
-    netAE.cuda()
-    #netAtt.cuda()
-    #netClf.cuda()
+    ae_model.cuda()
 
-#m = nn.Sequential(
-#   nn.AvgPool2d(kernel_size=(preData.data[1].shape[2], 1)),
-#    nn.Flatten(),
-#    nn.Flatten())
+# --------- Training loop ---------
+os.makedirs(args.save_dir, exist_ok=True)
 
-opt_netAE = optim.Adam(list(netAE.parameters()), lr=args.learning_rate_AE)
-
-print("Model Initialized.")
-
-# loss criteria
-criterion_reconstruction = nn.MSELoss(reduction = 'none')
-
-
-# setup logger
-with open(os.path.join(args.save_dir, 'log.txt'), 'w') as f:
-    print(args, file=f)
-    print(netAE, file=f)
-
-
-
-def train_autoencoders(pre_inputs, post_inputs):
-    
-    netAE.train()
-    
-
-    
-    # process input data
-    pre_inputs, post_inputs = Variable(pre_inputs), Variable(post_inputs)
-
-    if args.use_gpu:
-        pre_inputs, post_inputs = pre_inputs.cuda(), post_inputs.cuda()
-
-    # reset parameter gradients
-    netAE.zero_grad()
-
-    
-    # forward pass
-    pre_recon = netAE(pre_inputs)
-    post_recon = netAE(post_inputs)
-
-    
-
-    
-    # compute losses
-
-    loss = criterion_reconstruction(pre_recon, pre_inputs) + criterion_reconstruction(post_recon, post_inputs)
-
-
-    # backpropagate and update model
-    loss.backward()
-    opt_netAE.step()
-
-
-    
-    summary_stats = {'recon_loss1': loss.item()}
-    
-    
-    return summary_stats
-
-
-
-
-def accuracy(y_prob, y_true):
-    y_prob = y_prob > 0.5
-    return (y_true == y_prob).sum().item() / y_true.size(0)
-
-
-### main training loop
 for epoch in range(args.max_epochs):
-    print(epoch)
+    ae_model.train()
+    epoch_loss = 0
 
-    recon_loss = 0
-    clf_loss = 0
-    
+    for batch in data_loader:
+        batch = Variable(batch)
+        if args.use_gpu:
+            batch = batch.cuda()
 
-    for idx, (pre_inputs, post_inputs, clinic_inputs, targets) in enumerate(zip(preCyTOF_loader, postCyTOF_loader, clinic_loader, target_laoder)):
-        out = train_autoencoders(pre_inputs, post_inputs, clinic_inputs, targets)
-        recon_loss += out['recon_loss']
+        optimizer.zero_grad()
+        _, recon = ae_model(batch)
+	# maybe store `encoding.detach().cpu().numpy()` for later
+        loss = criterion(recon, batch)
+        loss.backward()
+        optimizer.step()
 
-    with open(os.path.join(args.save_dir, 'log.txt'), 'a') as f:
-        print('Epoch: ', epoch, ', recon loss : %.8f' % float(recon_loss), file=f)
 
-    # save model
+        epoch_loss += loss.item() * batch.size(0)
+
+    epoch_loss /= len(data_loader.dataset)
+    print(f"Epoch {epoch:03d} - Recon Loss: {epoch_loss:.6f}")
+
+    # Save model every frequency epochs
     if epoch % args.save_freq == 0:
-        torch.save(netAE.cpu().state_dict(), os.path.join(args.save_dir,"netAE_%s.pth" % epoch))
-
-    
-
-    if args.use_gpu:
-        netAE.cuda()
-
-        
-        
-        
-
+        torch.save(ae_model.state_dict(), os.path.join(args.save_dir, f"simpleAE_epoch{epoch}.pth"))
--- cytoGPNet model/train_simplified.py
+++ cytoGPNet model/train_simplified.py
@@ -12,8 +12,7 @@ from torch import nn, optim
 from torch.autograd import Variable
 
 from loaddata import CyTOF_Dataset
-from AE import Autoencoder, Attention_Layer, avg_pool
-from gp_utils import DGPXRLModel, CutstomizedGaussianLikelihood
+from loadmodel import simple_AE, GaussianProcessLayer, Attention_Layer, Simple_Classifier
 
 import os
 import argparse
@@ -21,27 +20,33 @@ import numpy as np
 
 torch.manual_seed(1)
 
-
 def setup_args():
 
     options = argparse.ArgumentParser()
 
+    # data directory
+    options.add_argument('-datadir', '--data-dir', action="store", dest="data_dir", default='./HEUvsUE')
+
     # save and directory options
-    options.add_argument('-sd', '--save-dir', action="store", dest="save_dir", default='./eval')
-    options.add_argument('--save-freq', action="store", dest="save_freq", default=20, type=int)
-    #options.add_argument('--pretrained-file', action="store")
+    options.add_argument('-sd', '--save-dir', action="store", dest="save_dir", default='./cytoGPNet_output')
+    options.add_argument('--save-freq', action="store", dest="save_freq", default=10, type=int)
+    #options.add_argument('--pretrained-file', action="store", dest="pretrained_file")
 
     # training parameters
     options.add_argument('-bs', '--batch-size', action="store", dest="batch_size", default=1, type=int)
     options.add_argument('-w', '--num-workers', action="store", dest="num_workers", default=10, type=int)
-    options.add_argument('-lrAE', '--learning-rate-AE', action="store", dest="learning_rate_AE", default=1e-4, type=float)
-    options.add_argument('-lrD', '--learning-rate-D', action="store", dest="learning_rate_D", default=1e-4, type=float)
-    options.add_argument('-e', '--max-epochs', action="store", dest="max_epochs", default=1001, type=int)
+    options.add_argument('-lrAE', '--learning-rate-AE', action="store", dest="learning_rate_AE", default=1e-6, type=float)
+    options.add_argument('-lrD', '--learning-rate-D', action="store", dest="learning_rate_D", default=1e-6, type=float)
+    options.add_argument('-e', '--max-epochs', action="store", dest="max_epochs", default=100, type=int)
     options.add_argument('-wd', '--weight-decay', action="store", dest="weight_decay", default=0, type=float)
 
+
     # hyperparameters
-    options.add_argument('--alpha', action="store", default=1., type=float) # weight for classfication loss compared to discriminative loss
-    options.add_argument('--latent-dims', action="store", default=4, type=int) # size of dimension for latent space of autoencoder
+    # options.add_argument('--alpha', action="store", default=1., type=float) # weight for classfication loss compared to discriminative loss
+    options.add_argument('--hidden-dims', action="store", dest="hidden_dims", default=4, type=int)
+    options.add_argument('--latent-dims', action="store", dest="latent_dims", default=2, type=int) # size of dimension for latent space of autoencoder
+    options.add_argument('--num-inducing-points', action="store", dest="num_inducing_points", default=100, type=int) # size of dimension for latent space of autoencoder
+
 
     # gpu options
     options.add_argument('-gpu', '--use-gpu', action="store_false", dest="use_gpu")
@@ -52,207 +57,96 @@ def setup_args():
 args = setup_args()
 if not torch.cuda.is_available():
     args.use_gpu = False
+device = torch.device("cuda" if args.use_gpu else "cpu")
 
 os.makedirs(args.save_dir, exist_ok=True)
 
 
 #============= TRAINING INITIALIZATION ==============
 
-# load data, shape (3, 1, 10000, 24)
-preData = CyTOF_Dataset(datadir="FCS_to_Array", name = "train_preData.obj", mode='train')
-postData = CyTOF_Dataset(datadir="FCS_to_Array",name = "train_postData.obj", mode='train')
-clinic_data = np.array(postData.data[0])[:,1].astype('float')
-# TODO: target_data should be label and it should be either 0 or 1.
-# target_data = np.array(postData.data[0])[:,1].astype('int') # I run into error at this line. Cannot convert str into int.I rewrote it with the correct syntex but the semantic seems wrong.
-# np.array(postData.data[0])[:,1] is ['study1', 'study2', 'study3'], converting it into int is [1, 2, 3], not 0 or 1.
-target_data = np.array([int(i[-1]) for i in np.array(postData.data[0])[:,2]])
+# ----- Load and preprocess data -----
+dataset = CyTOF_Dataset(datadir=args.data_dir, name="train_Data.obj", mode='train')
+cyto_tensor = torch.from_numpy(dataset.data[1]).float()  # (n_samples, n_channels, n_cells, n_markers)
 
-p = preData.data[0].shape[1] - 2
-
-preCyTOF_loader = torch.utils.data.DataLoader(torch.from_numpy(preData.data[1]).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-postCyTOF_loader = torch.utils.data.DataLoader(torch.from_numpy(postData.data[1]).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-clinic_loader = torch.utils.data.DataLoader(torch.from_numpy(clinic_data).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-target_laoder = torch.utils.data.DataLoader(torch.from_numpy(target_data).float(), batch_size=args.batch_size, drop_last=True, shuffle=False)
-
-print("Data loaded.")
+N, C, T, M = cyto_tensor.shape
+cyto_concat = cyto_tensor.permute(0, 2, 1, 3).reshape(N, C * T, M)  # (n_samples, n_channels * n_cells, n_markers)
+labels = torch.tensor(dataset.data[0]["label"].values).float()  # (n_samples,)
 
 
-# initialize autoencoder
-# TODO: maybe let netPre1 and netPost1 share parameters; let netPre2 and netPost2 share parameters (Do this later).
-# TODO: since you add the pooling layer to the classifier, you can still use the reconstruction error for these encoders  (Do this later).
-
-netAE = Autoencoder(n_input=preData.data[1].shape, nz=args.latent_dims) # common encoder
-netAE.load_state_dict(torch.load(os.path.join(args.save_dir, "netAE_%s.pth" % args.max_epochs)))
-netGP = DGPXRLModel(netAE, preData, postData)
-netAtt1 = Attention_layer(n_input=preData.data[1])
-netAtt2 = Attention_layer(n_input=postData.data[1])
-netClf = Simple_Classifier(nz = p + 2 * args.latent_dims)
-
-if args.use_gpu:
-    netAE.cuda()
-    netAtt1.cuda()
-    netAtt2.cuda()
-    netClf.cuda()
-
-m = nn.Sequential(
-    nn.AvgPool2d(kernel_size=(preData.data[1].shape[2], 1)),
-    nn.Flatten(),
-    nn.Flatten())
-
-opt_netAE = optim.Adam(list(netAE.parameters()), lr=args.learning_rate_AE)
-opt_netGP = optim.Adam(list(netAE.parameters()), lr=args.learning_rate_AE)
-opt_netAtt1 = optim.Adam(list(netAtt1.parameters()), lr=args.learning_rate_AE)
-opt_netAtt2 = optim.Adam(list(netAtt2.parameters()), lr=args.learning_rate_AE)
-opt_netClf = optim.Adam(list(netClf.parameters()), lr=args.learning_rate_D, weight_decay=args.weight_decay)
-
-print("Model Initialized.")
-
-# loss criteria
-criterion_discriminate= nn.MSELoss(reduction = 'none')
-criterion_classify = nn.BCELoss()
+# Dataset
+data_loader = DataLoader(TensorDataset(cyto_concat, labels), batch_size=args.batch_size, shuffle=True)
 
 
-# setup logger
-with open(os.path.join(args.save_dir, 'log.txt'), 'w') as f:
-    print(args, file=f)
-    print(netAE, file=f)
-    print(netAtt1, file=f)
-    print(netAtt2, file=f)
-    print(netClf, file=f)
+# ----- Load pretrained AE -----
+ae_path = args.pretrained_file
+autoencoder = simple_AE(input_dim=M, embed_dim=args.latent_dims, hidden_dim=args.hidden_dims).to(device)
+autoencoder.load_state_dict(torch.load(ae_path))
+autoencoder.train()  # Unfreeze for fine-tuning
 
+# ----- Initialize attention + classifier -----
+attention = Attention_Layer().to(device)
+classifier = Simple_Classifier(nz=1).to(device)
 
+# ----- Prepare GP Layer -----
+# Sample a subset of encoded points to initialize inducing points
+with torch.no_grad():
+    flat = cyto_concat[:args.num_inducing_points].reshape(-1, M).to(device)
+    z_induce, _ = autoencoder(flat)
+    z_induce = z_induce.view(-1, 1)
 
+gp_layer = GaussianProcessLayer(
+    input_dim=1,
+    num_inducing_points=z_induce.size(0),
+    inducing_points=z_induce.clone(),
+    mean_inducing_points=z_induce.clone(),
+    grid_bounds=[(z_induce.min().item(), z_induce.max().item())],
+    likelihood_type='classification'
+).to(device)
 
-def train_classifier(pre_inputs, post_inputs, clinic_inputs, targets):
+likelihood = gpytorch.likelihoods.BernoulliLikelihood().to(device)
+mll = gpytorch.mlls.VariationalELBO(likelihood, gp_layer, num_data=N)
 
-    netAE.eval()
-    netGP.train()
-    netAtt1.train()
-    netAtt2.train()
-    netClf.train()
+# ----- Optimizer -----
+optimizer = torch.optim.Adam(
+    list(gp_layer.parameters()) + list(attention.parameters()) + list(classifier.parameters()),
+    lr=args.learning_rate_D
+)
 
-    # process input data
-    pre_inputs, post_inputs, clinic_inputs, targets = Variable(pre_inputs), Variable(post_inputs), Variable(clinic_inputs), Variable(targets)
-
-    if args.use_gpu:
-        pre_inputs, post_inputs, clinic_inputs, targets = pre_inputs.cuda(), post_inputs.cuda(), clinic_inputs.cuda(), targets.cuda()
-
-    # reset parameter gradients
-    netGP.zero_grad()
-    netAtt1.zero_grad()
-    netAtt2.zero_grad()
-    netClf.zero_grad()
-
-    # forward pass
-    pre_latent = netAE.encoder(pre_inputs)
-    #pre_latent2 = netPre2(pre_inputs)
-    post_latent = netAE.encoder(post_inputs)
-    #post_latent2 = netPost2(post_inputs)
-    latent = netGP(torch.concat([pre_latent, post_latent], dim = 1))
-    pre_output = avg_pool(netAtt1(latent[:, :pre_inputs.size(1)]))
-    post_output = avg_pool(netAtt2(latent[:, pre_inputs.size(1):post_inputs.size(1)]))
-    d = torch.cat((pre_output, post_output, clinic_inputs), dim = 1)
-    scores = netClf(d)
-
-
-    # compute losses
-    clf_loss = criterion_classify(scores, targets)
-
-    loss = args.beta*clf_loss
-
-    # backpropagate and update model
-    loss.backward()
-    opt_netClf.step()
-
-    summary_stats = {'clf_loss': clf_loss, 'accuracy': accuracy(scores, targets)}
-
-    return summary_stats
-
-
-def train_general(pre_inputs, post_inputs, clinic_inputs, targets):
-    netAE.train()
-    netGP.train()
-    netAtt1.train()
-    netAtt2.train()
-    netClf.train()
-
-    # process input data
-    pre_inputs, post_inputs, clinic_inputs, targets = Variable(pre_inputs), Variable(post_inputs), Variable(
-        clinic_inputs), Variable(targets)
-
-    if args.use_gpu:
-        pre_inputs, post_inputs, clinic_inputs, targets = pre_inputs.cuda(), post_inputs.cuda(), clinic_inputs.cuda(), targets.cuda()
-
-    # reset parameter gradients
-    netGP.zero_grad()
-    netAtt1.zero_grad()
-    netAtt2.zero_grad()
-    netClf.zero_grad()
-
-    # forward pass
-    pre_latent = netAE.encoder(pre_inputs)
-    # pre_latent2 = netPre2(pre_inputs)
-    post_latent = netAE.encoder(post_inputs)
-    # post_latent2 = netPost2(post_inputs)
-    latent = netGP(torch.concat([pre_latent, post_latent], dim=1))
-    pre_output = avg_pool(netAtt1(latent[:, :pre_inputs.size(1)]))
-    post_output = avg_pool(netAtt2(latent[:, pre_inputs.size(1):post_inputs.size(1)]))
-    d = torch.cat((pre_output, post_output, clinic_inputs), dim=1)
-    scores = netClf(d)
-
-    # compute losses
-    clf_loss = criterion_classify(scores, targets)
-
-    loss = args.beta * clf_loss
-
-    # backpropagate and update model
-    loss.backward()
-    opt_netClf.step()
-
-    summary_stats = {'clf_loss': clf_loss, 'accuracy': accuracy(scores, targets)}
-
-    return summary_stats
-
-
-def accuracy(y_prob, y_true):
-    y_prob = y_prob > 0.5
-    return (y_true == y_prob).sum().item() / y_true.size(0)
-
-
-### main training loop
+# ----- Training Loop -----
 for epoch in range(args.max_epochs):
-    print(epoch)
+    total_loss = 0
+    gp_layer.train(); likelihood.train(); attention.train(); classifier.train(); autoencoder.train()
 
-    recon_loss = 0
-    clf_loss = 0
-    
+    for x_batch, y_batch in tqdm(data_loader, desc=f"Epoch {epoch+1}"):
+        x_batch = x_batch.to(device)  # (B, T, M)
+        y_batch = y_batch.to(device)
 
-    for idx, (pre_inputs, post_inputs, clinic_inputs, targets) in enumerate(zip(preCyTOF_loader, postCyTOF_loader, clinic_loader, target_laoder)):
-        out = train_general(pre_inputs, post_inputs, clinic_inputs, targets)
-        clf_loss += out['clf_loss']
-        
-    with open(os.path.join(args.save_dir, 'log.txt'), 'a') as f:
-        print('Epoch: ', epoch, ', disc loss 1: %.8f' % float(disc_loss1),
-                ', clf loss: %.8f' % float(clf_loss), file=f)
+        # Encode cells via AE (no gradient)
+        with torch.no_grad():
+            z, _ = autoencoder(x_batch.reshape(-1, M))  # (B*T, 1)
+        z = z.view(x_batch.size(0), -1, 1)  # (B, T, 1)
 
-    # save model
+        # GP layer forward
+        output = gp_layer(z)
+        f_samples = output.rsample().squeeze(-1)  # (B, T)
+
+        # Attention -> pooled sample
+        pooled = attention(f_samples)  # (B,)
+        preds = classifier(pooled.unsqueeze(-1)).squeeze(-1)  # (B,)
+
+        # Binary classification loss
+        loss = F.binary_cross_entropy(preds, y_batch)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+    # Save model every frequency epochs
     if epoch % args.save_freq == 0:
-        torch.save(netAE.cpu().state_dict(), os.path.join(args.save_dir,"netAE_%s.pth" % epoch))
-        torch.save(netGP.cpu().state_dict(), os.path.join(args.save_dir,"netGP_%s.pth" % epoch))
-        torch.save(netAtt1.cpu().state_dict(), os.path.join(args.save_dir,"netAtt1_%s.pth" % epoch))
-        torch.save(netAtt2.cpu().state_dict(), os.path.join(args.save_dir,"netAtt2_%s.pth" % epoch))
-        torch.save(netClf.cpu().state_dict(), os.path.join(args.save_dir,"netClf_%s.pth" % epoch))
-    
-
-    if args.use_gpu:
-        netAE.cuda()
-        netGP.cuda()
-        netAtt1.cuda()
-        netAtt2.cuda()
-        netClf.cuda()
-        
-        
-        
-        
-        
-
+        torch.save(autoencoder.state_dict(), os.path.join(args.save_dir, f"simpleAE_finetune_epoch{epoch}.pth"))
+        torch.save(gp_layer.state_dict(), os.path.join(args.save_dir, f"GaussianProcessLayer_epoch{epoch}.pth"))
+        torch.save(likelihood.state_dict(), os.path.join(args.save_dir, f"BernoulliLikelihood_epoch{epoch}.pth"))
+        torch.save(attention.state_dict(), os.path.join(args.save_dir, f"Attention_Layer_epoch{epoch}.pth"))
+        torch.save(classifier.state_dict(), os.path.join(args.save_dir, f"Simple_Classifier_epoch{epoch}.pth"))
+    print(f"Epoch {epoch+1} | Avg Loss: {total_loss / len(data_loader):.4f}")
